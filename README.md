97f7013de0f63b95534e2fb259936ca087f17a27
lippzhang <lippzhang@tencent.com>
2022-05-11 17:42:40 +0800

    feat: tdd

---
+++ README.md
@@ -0,0 +1,6 @@
+# minigrep
+
+video address:
+* [Writing a CLI App in Rust! - Part 1](https://www.youtube.com/watch?v=XYkiwsplDTg&list=PLai5B987bZ9CoVR-QEIN9foz4QCJ0H2Y8&index=15) 
+
+* [Writing a CLI App in Rust! - Part 2](https://www.youtube.com/watch?v=AABHxixn6Cw&list=PLai5B987bZ9CoVR-QEIN9foz4QCJ0H2Y8&index=16) 
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,20 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     println!("search for {}", config.query);
     println!("in file {}", config.filename);
 
-    if let Err(err) = run(config) {
-        println!("application error: {}", err);
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("application error: {}", err);
         process::exit(1);
-    }
-}
-fn run(config: Config) -> Result<(),Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("Read file \n {}", contents);
-    Ok(())
-}
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config{ query, filename })
     }
 }
